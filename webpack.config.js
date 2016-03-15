f00a2613676b42db36af4ddd71c29136333a10d6
zendy <zendywongso@gmail.com>
2016-03-15 23:50:28 +1100

    Add babel

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,25 @@ module.exports = {
     path: PATHS.dist
   },
   devServer: {
-    contentBase: PATHS.join,
+    contentBase: PATHS.dist,
     port: 8080,
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules|bower_components)/,
+        // Enable caching for improved performance during development
+        // It uses default OS directory by default. If you need something
+        // more custom, pass a path to it. I.e., babel?cacheDirectory=<path>
+        loader: 'babel',
+        // Parse only app files! Without this it will go through entire project.
+        // In addition to being slow, that will most likely result in an error.
+        include: PATHS.app,
+        query: {
+          presets: 'es2015'
+        }
+      }
+    ]
   }
 };

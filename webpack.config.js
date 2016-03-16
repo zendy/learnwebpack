6a9d059502db51542670317b7a1273c9873ed046
zendy <zendywongso@gmail.com>
2016-03-16 11:41:47 +1100

    Add React Hot Loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const webpack = require('webpack');
 
+const TARGET = process.env.npm_lifecycle_event;
+process.env.BABEL_ENV = TARGET;
+
 const PATHS = {
   app: path.join(__dirname, 'app'),
   dist: path.join(__dirname, 'dist')
